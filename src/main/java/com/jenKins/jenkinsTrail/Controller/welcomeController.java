158833595e558bb87269098adb8caed797af06d1
Pirathees <kamalanathanpirathees@gmail.com>
2022-02-20 23:02:48 +0530

    Second Commit

--- src/main/java/com/jenKins/jenkinsTrail/Controller/welcomeController.java
+++ src/main/java/com/jenKins/jenkinsTrail/Controller/welcomeController.java
@@ -1,14 +1,21 @@
 package com.jenKins.jenkinsTrail.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableScheduling
 public class welcomeController {
+    Logger logger = LoggerFactory.getLogger(welcomeController.class);
 
     @RequestMapping(value = "/welcome",method = RequestMethod.GET)
     public String welcomeMethod(){
         return "Hello From jenkins Build";
     }
+
+
 }
--- src/main/java/com/jenKins/jenkinsTrail/JenkinsTrailApplication.java
+++ src/main/java/com/jenKins/jenkinsTrail/JenkinsTrailApplication.java
@@ -3,11 +3,14 @@ package com.jenKins.jenkinsTrail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class JenkinsTrailApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTrailApplication.class, args);
+		System.out.println("Welcome From Jenkins Version 1");
 	}
 
 }
