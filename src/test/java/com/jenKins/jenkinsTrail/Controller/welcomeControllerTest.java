4d727c795f4c20f89dfa76708d355ca360646d50
Pirathees <kamalanathanpirathees@gmail.com>
2022-02-20 23:35:22 +0530

    First Test Added

---
+++ src/test/java/com/jenKins/jenkinsTrail/Controller/welcomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.jenKins.jenkinsTrail.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class welcomeControllerTest {
+    Logger logger = LoggerFactory.getLogger(welcomeControllerTest.class);
+    @Test
+    void welcomeMethod() {
+        assertEquals("david","david","First Failed");
+        logger.info("Testing started Da Pts");
+    }
+}
