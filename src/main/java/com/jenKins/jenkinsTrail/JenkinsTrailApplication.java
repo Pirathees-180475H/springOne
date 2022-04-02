798b8b751c451adfe8d4cd0fce8f5ac4a9a83610
Pirathees <75510813+Pirathees-180475H@users.noreply.github.com>
2022-04-02 22:51:20 +0530

    Update JenkinsTrailApplication.java

--- src/main/java/com/jenKins/jenkinsTrail/JenkinsTrailApplication.java
+++ src/main/java/com/jenKins/jenkinsTrail/JenkinsTrailApplication.java
@@ -5,12 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class JenkinsTrailApplication {
+public class JenkinsTrailApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTrailApplication.class, args);
 		System.out.println("Welcome From Jenkins Version 1");
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
 	}
 
 }
